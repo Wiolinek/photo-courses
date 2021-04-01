9e83397abe3d52d7f8da3e8cc7cb42e74dfc7fe7
Wiola P <wiol@onet.eu>
2021-04-02 01:14:30 +0200

    footer basic style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,21 @@ import '../styles/footer.css';
 const Footer= () => {
   return (
       <footer>
+        <div>
+          <i class="fab fa-facebook-f"></i>
+        </div>
+        <div>
+          <i class="fab fa-instagram"></i>
+        </div>
+        <div>
+          <i class="fab fa-twitter"></i>
+        </div>
+        <div>
+          <i class="fab fa-pinterest"></i>
+        </div>
+        <div>
+          <i class="fab fa-youtube"></i>
+        </div>
       </footer>
   );
 }
