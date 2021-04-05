194447e0fd992d6d3242beede2c171469076a1be
Wiola P <wiol@onet.eu>
2021-04-05 19:27:08 +0200

    map fix and style

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,18 @@
 import GoogleMapReact from 'google-map-react';
 
-const Map = () => {
+import '../styles/map.css';
 
-    let Test = ({ text }) => <div>{text}</div>;
+const Map = ( { lat, lng }) => {
+
+    let Label = () => <svg className='map-marker' id="i-location" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34 34" width="34" height="34" fill="none" stroke="currentcolor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
+    <circle cx="16" cy="11" r="4" />
+    <path d="M24 15 C21 22 16 30 16 30 16 30 11 22 8 15 5 8 10 2 16 2 22 2 27 8 24 15 Z" />
+</svg>;
 
     const defaultProps = {
         center: {
-          lat: 59.95,
-          lng: 30.33
+          lat: lat,
+          lng: lng,
         },
         zoom: 11
       };  
@@ -19,10 +24,9 @@ const Map = () => {
                 defaultCenter={defaultProps.center}
                 defaultZoom={defaultProps.zoom}
             >
-            <Test
-                lat={59.955413}
-                lng={30.337844}
-                text="photo passion"
+            <Label
+                lat={lat}
+                lng={lng}
             />
             </GoogleMapReact>
       </div>
