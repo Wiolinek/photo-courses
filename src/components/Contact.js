d82127bc2390a369767c6788200871649fb47cfa
Wiola P <wiol@onet.eu>
2021-04-05 19:28:20 +0200

    map atyle & send mesage section add

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,12 +4,17 @@ import Map from './Map';
 
 const Contact = () => {
 
+    const latBerlin = 52.50082077239646;
+    const lngBerlin = 13.348808727264174;
+    const latHamburg = 53.548712766408364;
+    const lngHamburg = 9.945696156442432;
+
     return (
         <section className='contact'>
             <h1>Contact Us...</h1>
             <svg className="section-circle" width="200" height="200" viewBox="0 0 242 242" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M161 70C161 108.66 129.66 140 91 140C52.3401 140 21 108.66 21 70C21 31.3401 52.3401 0 91 0C129.66 0 161 31.3401 161 70Z" fill="#FF0000" fill-opacity="0.25"/>
-            <path d="M140 172C140 210.66 108.66 242 70 242C31.3401 242 1.64986e-06 210.66 1.64986e-06 172C1.64986e-06 133.34 31.3401 102 70 102C108.66 102 140 133.34 140 172Z" fill="#0202FF" fill-opacity="0.25"/><circle cx="170" cy="140" r="70" transform="rotate(-1.63957 170 140)" fill="#169216" fill-opacity="0.25"/>
+            <path d="M161 70C161 108.66 129.66 140 91 140C52.3401 140 21 108.66 21 70C21 31.3401 52.3401 0 91 0C129.66 0 161 31.3401 161 70Z" fill="#FF0000" fillOpacity="0.25"/>
+            <path d="M140 172C140 210.66 108.66 242 70 242C31.3401 242 1.64986e-06 210.66 1.64986e-06 172C1.64986e-06 133.34 31.3401 102 70 102C108.66 102 140 133.34 140 172Z" fill="#0202FF" fillOpacity="0.25"/><circle cx="170" cy="140" r="70" transform="rotate(-1.63957 170 140)" fill="#169216" fillOpacity="0.25"/>
             </svg>
             <p className='contact__intro'>We are at your service in two following locations:</p>
             <article className='contact__container'>
@@ -28,8 +33,8 @@ const Contact = () => {
                         <p>contact-berlin@photo-passion.com</p>
                     </div>
                 </div>
-                <div className='contact__map'>
-                    <Map />
+                <div>
+                    <Map lat={latBerlin} lng={lngBerlin}/>
                 </div>
             </article>
             <article className='contact__container'>
@@ -48,10 +53,16 @@ const Contact = () => {
                         <p>contact-hamburg@photo-passion.com</p>
                     </div>
                 </div>
-                <div className='contact__map'>
-                    <Map />
+                <div>
+                    <Map lat={latHamburg} lng={lngHamburg}/>
                 </div>
             </article>
+            <form className='contact__form'>
+                <label>Name<br/><input type="text" name="name"></input></label>
+                <label>Email<br/><input type="email" name="mail"></input></label>
+                <label>Subject<br/><input type="text" name="subject"></input></label>
+                <label>Message<br/><textarea name="message" rows="6" cols="50"></textarea></label>
+            </form>
         </section>
     )
     
