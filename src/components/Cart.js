ba8a9686e5d4847346d5fbad4985ecb22aad804d
Wiola P <wiol@onet.eu>
2021-04-25 18:38:47 +0200

    cart style & total amount & items quantity implem.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,8 @@
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { increaseQuantity, decreaseQuantity, deleteFromCart } from '../redux/cartSlice';
 
 import { NavLink } from "react-router-dom";
+
 
 
 import '../styles/cart.css';
@@ -10,6 +10,7 @@ import '../styles/cart.css';
 const Cart = () => {
 
     const { cart } = useSelector((state) => state.cart);
+    const totalAmount = useSelector((state) => state.cart.totalAmount);
     const dispatch = useDispatch();
 
     const coursesList = cart.map(course => 
@@ -20,7 +21,7 @@ const Cart = () => {
                 <p>{course.quantity}</p>
                 <button className="cart__btn" onClick={() => dispatch(increaseQuantity(course))}><svg id="i-plus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="none" stroke="currentcolor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"><path d="M16 2 L16 30 M2 16 L30 16" /></svg></button>
             </div>
-            <p className="cart__price">{course.price} EUR</p>
+            <p className="cart__price">{course.quantity * course.price} EUR</p>
             <div className="cart__menu-remove">
                 <button className="cart__btn" onClick={() => dispatch(deleteFromCart(course))}>
                 <svg id="i-trash" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="20" height="20" fill="none" stroke="currentcolor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"><path d="M28 6 L6 6 8 30 24 30 26 6 4 6 M16 12 L16 24 M21 12 L20 24 M11 12 L12 24 M12 6 L13 2 19 2 20 6" /></svg>
@@ -31,12 +32,20 @@ const Cart = () => {
     return (
         <section className="cart">
             <h1>You are now only one step from great photo adventure...</h1>
-            <div className="cart__menu"><p className="cart__item-name">Course name</p><p className="cart__quantity">Quantity</p><p className="cart__price">Price</p><p className="cart__menu-remove">Remove</p></div>
+            <div className="cart__menu">
+                <p className="cart__item-name">Course name</p>
+                <p className="cart__quantity">Quantity</p>
+                <p className="cart__price">Price</p>
+                <p className="cart__menu-remove">Remove</p>
+            </div>
             <ul className="cart__list">
                 {coursesList}
             </ul>
-            <p>Total price: EUR</p>
-            <div className="cart__btn--section"><NavLink className="cart__btn cart__btn--large" to="/our-courses">Continue shopping</NavLink><button className="cart__btn cart__btn--large">Checkout</button></div>
+            <p  className="cart__total-price">Total price: {totalAmount} EUR</p>
+            <div className="cart__btn--section">
+                <NavLink className="cart__btn cart__btn--large" to="/our-courses">Continue shopping</NavLink>
+                <button className="cart__btn cart__btn--large">Checkout</button>
+            </div>
         </section>
     )
     
--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -2,13 +2,17 @@ import { createSlice } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+
 export const cartSlice = createSlice({
   name: 'cart',
   initialState: {
     cart: [],
+    itemsQuantity: 0,
+    totalAmount: 0,
   },
 
   reducers: {
+    
     addToCart: (state, { payload }) => {
 
       if(state.cart.length > 0) {
@@ -16,37 +20,51 @@ export const cartSlice = createSlice({
         const existItem = state.cart.find(item => payload.id === item.id);
 
         if(existItem) {
-          state.cart = state.cart.map(item => item === existItem ? {...existItem, quantity: existItem.quantity + 1} : item)
+          state.cart = state.cart.map(item => item === existItem ? {...existItem, quantity: existItem.quantity + 1} : item);
+          state.itemsQuantity = state.itemsQuantity + 1;
+          state.totalAmount = state.totalAmount + payload.price;
 
         } else if (!existItem) {
-          state.cart.push({...payload, quantity: 1})
+          state.cart.push({...payload, quantity: 1});
+          state.itemsQuantity = state.itemsQuantity + 1;
+          state.totalAmount = state.totalAmount + payload.price;
         }
         
       } else {
-        state.cart.push({...payload, quantity: 1})
+        state.cart.push({...payload, quantity: 1});
+        state.itemsQuantity = state.itemsQuantity + 1;
+        state.totalAmount = state.totalAmount + payload.price;
       }
     },
 
     deleteFromCart: (state, { payload }) => {
       state.cart = state.cart.filter(course => course.id !== payload.id)
+      state.itemsQuantity = state.itemsQuantity - payload.quantity;
+      state.totalAmount = state.totalAmount - (payload.price * payload.quantity);
     },
 
     increaseQuantity: (state, { payload }) => {
-      state.cart = state.cart.map(item => item.id === payload.id ? {...item, quantity: item.quantity + 1} : item)
+      state.cart = state.cart.map(item => item.id === payload.id ? {...item, quantity: item.quantity + 1} : item);
+      state.itemsQuantity = state.itemsQuantity + 1;
+      state.totalAmount = state.totalAmount + payload.price;
     },
     decreaseQuantity: (state, { payload }) => {
 
       if(payload.quantity === 1) {
-        state.cart = state.cart.filter(course => course.id !== payload.id)
+        state.cart = state.cart.filter(course => course.id !== payload.id);
+        state.itemsQuantity = state.itemsQuantity - 1;
+        state.totalAmount = state.totalAmount - payload.price;
 
       } else {
-        state.cart = state.cart.map(item => item.id === payload.id ? {...item, quantity: item.quantity - 1} : item)
+        state.cart = state.cart.map(item => item.id === payload.id ? {...item, quantity: item.quantity - 1} : item);
+        state.itemsQuantity = state.itemsQuantity - 1;
+        state.totalAmount = state.totalAmount - payload.price;
       }
     },
   },
 })
 
-// Action creators are generated for each case reducer function
+// Action creators generated for each reducer function
 export const { addToCart, deleteFromCart, increaseQuantity, decreaseQuantity } = cartSlice.actions;
 
 const rootReducer = cartSlice.reducer;
