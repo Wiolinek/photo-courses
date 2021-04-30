2016466c145e86a5aceac68f4614b31214555cc9
Wiola P <wiol@onet.eu>
2021-04-30 13:38:34 +0200

    course_id fix

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -17,7 +17,7 @@ export const cartSlice = createSlice({
 
       if(state.cart.length > 0) {
 
-        const existItem = state.cart.find(item => payload.id === item.id);
+        const existItem = state.cart.find(item => payload.course_id === item.course_id);
 
         if(existItem) {
           state.cart = state.cart.map(item => item === existItem ? {...existItem, quantity: existItem.quantity + 1} : item);
@@ -38,25 +38,25 @@ export const cartSlice = createSlice({
     },
 
     deleteFromCart: (state, { payload }) => {
-      state.cart = state.cart.filter(course => course.id !== payload.id)
+      state.cart = state.cart.filter(course => course.course_id !== payload.course_id)
       state.itemsQuantity = state.itemsQuantity - payload.quantity;
       state.totalAmount = state.totalAmount - (payload.price * payload.quantity);
     },
 
     increaseQuantity: (state, { payload }) => {
-      state.cart = state.cart.map(item => item.id === payload.id ? {...item, quantity: item.quantity + 1} : item);
+      state.cart = state.cart.map(item => item.course_id === payload.course_id ? {...item, quantity: item.quantity + 1} : item);
       state.itemsQuantity = state.itemsQuantity + 1;
       state.totalAmount = state.totalAmount + payload.price;
     },
     decreaseQuantity: (state, { payload }) => {
 
       if(payload.quantity === 1) {
-        state.cart = state.cart.filter(course => course.id !== payload.id);
+        state.cart = state.cart.filter(course => course.course_id !== payload.course_id);
         state.itemsQuantity = state.itemsQuantity - 1;
         state.totalAmount = state.totalAmount - payload.price;
 
       } else {
-        state.cart = state.cart.map(item => item.id === payload.id ? {...item, quantity: item.quantity - 1} : item);
+        state.cart = state.cart.map(item => item.course_id === payload.course_id ? {...item, quantity: item.quantity - 1} : item);
         state.itemsQuantity = state.itemsQuantity - 1;
         state.totalAmount = state.totalAmount - payload.price;
       }
