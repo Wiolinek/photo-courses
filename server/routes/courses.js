7a311abfe55fbc42c52d8f673d2e591754cf5df9
Wiola P <wiol@onet.eu>
2021-05-12 23:47:51 +0200

    teacher_name change into teacher

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var mysql = require('./../lib/mysql')
 
 router.get('/', function(req, res, next) {
-  mysql.query('SELECT name, level, duration, date, place, price, photo_course, course_id, teacher_name FROM courses LEFT JOIN courses_x_teachers ON courses.id = courses_x_teachers.course_id LEFT JOIN teachers ON courses_x_teachers.teacher_id = teachers.id', function(error, result, fields) {
+  mysql.query('SELECT name, level, duration, date, place, price, photo_course, course_id, teacher FROM courses LEFT JOIN courses_x_teachers ON courses.id = courses_x_teachers.course_id LEFT JOIN teachers ON courses_x_teachers.teacher_id = teachers.id', function(error, result, fields) {
     res.json(result);
   });
 });
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 router.post('/filter', function(req, res, next) {
   let filterObject = [];
   if(req.body.teacherFilter !== "All" && req.body.teacherFilter.length) {
-    filterObject.push(" teacher_name = '" + req.body.teacherFilter + "' ");
+    filterObject.push(" teacher = '" + req.body.teacherFilter + "' ");
   }
   if(req.body.levelFilter !== "All" && req.body.levelFilter.length) {
     filterObject.push(" level = '" + req.body.levelFilter + "' ");
@@ -21,7 +21,7 @@ router.post('/filter', function(req, res, next) {
     filterObject.push(" place = '" + req.body.locationFilter + "' ");
   }
 
-  mysql.query("SELECT name, level, duration, date, place, price, photo_course, course_id, teacher_name FROM courses LEFT JOIN courses_x_teachers ON courses.id = courses_x_teachers.course_id LEFT JOIN teachers ON courses_x_teachers.teacher_id = teachers.id " + (filterObject.length ? 'WHERE' + filterObject.join('AND') :''), function(error, result, fields) {
+  mysql.query("SELECT name, level, duration, date, place, price, photo_course, course_id, teacher FROM courses LEFT JOIN courses_x_teachers ON courses.id = courses_x_teachers.course_id LEFT JOIN teachers ON courses_x_teachers.teacher_id = teachers.id " + (filterObject.length ? 'WHERE' + filterObject.join('AND') :''), function(error, result, fields) {
     res.json(result);
   });
 });
