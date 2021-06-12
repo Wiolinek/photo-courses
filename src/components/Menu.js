d3067c0ddd497f7ebb1c45cc00991b4770e7ad50
Wiola P <wiol@onet.eu>
2021-06-12 21:08:16 +0200

    nav btns hiding menu fix

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,10 +10,10 @@ const Menu = ( { menuState, setMenuState } ) => {
     return (
         <div className="menu" style={{display: menuState ? "flex" : "none"}}>
             <ul className="menu__list">
-                <li className="menu__item" onClick={() => setMenuState()}><NavLink to="/about-us">About Us</NavLink></li>
-                <li className="menu__item" onClick={() => setMenuState()}><NavLink to="/our-teachers">Our Teachers</NavLink></li>
-                <li className="menu__item" onClick={() => setMenuState()}><NavLink to="/our-courses">Our Courses</NavLink></li>
-                <li className="menu__item" onClick={() => setMenuState()}><NavLink to="/contact">Contact</NavLink></li>
+                <li className="menu__item" onClick={() => setMenuState(false)}><NavLink to="/about-us">About Us</NavLink></li>
+                <li className="menu__item" onClick={() => setMenuState(false)}><NavLink to="/our-teachers">Our Teachers</NavLink></li>
+                <li className="menu__item" onClick={() => setMenuState(false)}><NavLink to="/our-courses">Our Courses</NavLink></li>
+                <li className="menu__item" onClick={() => setMenuState(false)}><NavLink to="/contact">Contact</NavLink></li>
             </ul>
         </div>
     )
