9f59aa0d83b6647f1b86b542e7cee79b6ee343da
Wiola P <wiol@onet.eu>
2021-06-12 20:35:58 +0200

    login btns style

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -20,7 +20,7 @@ const LogIn = () => {
                     <label>Email<br/><input type="email" name="mail" required></input></label>
                     <label>Password<br/><input type="password" name="password" required></input></label>
                     <label>Confirm password<br/><input type="password" name="confirm password" required></input></label>
-                    <button className="btn login__btn" type="submit">Register</button>
+                    <button className="login__btn" type="submit">Register</button>
                 </form>
             </div>
             <div className="login__section">
@@ -28,7 +28,7 @@ const LogIn = () => {
                 <form className="login__form">
                     <label>Email<br/><input type="email" name="mail" required></input></label>
                     <label>Password<br/><input type="password" name="password" required></input></label>
-                    <button className="btn login__btn" type="submit">LogIn</button>
+                    <button className="login__btn" type="submit">LogIn</button>
                     <NavLink className="login-section__reset-password" to="/">Reset password</NavLink>
                 </form>
             </div>
