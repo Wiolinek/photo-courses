0790c22ceeacb0612428575ed567257356bbf6c0
Wiola P <wiol@onet.eu>
2021-06-12 23:45:15 +0200

    aour courses intro text add

--- src/components/OurCourses.js
+++ src/components/OurCourses.js
@@ -35,7 +35,7 @@ const OurCourses = () => {
             <path d="M161 70C161 108.66 129.66 140 91 140C52.3401 140 21 108.66 21 70C21 31.3401 52.3401 0 91 0C129.66 0 161 31.3401 161 70Z" fill="#FF0000" fillOpacity="0.25"/>
             <path d="M140 172C140 210.66 108.66 242 70 242C31.3401 242 1.64986e-06 210.66 1.64986e-06 172C1.64986e-06 133.34 31.3401 102 70 102C108.66 102 140 133.34 140 172Z" fill="#0202FF" fillOpacity="0.25"/><circle cx="170" cy="140" r="70" transform="rotate(-1.63957 170 140)" fill="#169216" fillOpacity="0.25"/>
             </svg>
-            <p className="section-intro">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit ullam ex eos sunt cupiditate eligendi odit ratione obcaecati saepe non rerum voluptatibus harum commodi, delectus odio doloribus, recusandae deleniti fuga. Minus praesentium quidem nam quae error blanditiis nesciunt nostrum dolore. Ut quam nemo vero impedit est quod sit commodi, ad qui at inventore adipisci laborum labore fugiat dolore neque non pariatur id voluptas rem, deserunt quidem vitae! Esse, id reiciendis accusantium assumenda ipsa aliquid asperiores, voluptates quisquam iusto pariatur suscipit culpa sit laboriosam molestiae excepturi qui repellendus iste, quasi similique! Hic quaerat enim labore ullam nobis deleniti, ratione maiores soluta.</p>
+            <p className="section-intro">Learn from the best photography mentora with many years of experience. We prepared a large variety of courses for all skill levels. Choose from below courses the perfect one for you. Enjoy a lesson with a mentor in a friendly group and put your knowledge into practice.</p>
             <OurCoursesFilters />
             <div className="courses__section">
                 {coursesList.length ? 
