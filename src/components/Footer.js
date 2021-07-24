8b644e7c3f30b6154fe82db427c3c00dcfacdd72
Wiola P <wiol@onet.eu>
2021-07-24 12:31:51 +0200

    all rights reserved add + small style fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,18 +8,13 @@ const Footer= () => {
     <footer>
       <div>
         <a href="https://www.facebook.com" rel="noreferrer" target="_blank"><i className="fab fa-facebook-f"></i></a>
-      </div>
-      <div>
         <a href="https://www.instagram.com" rel="noreferrer" target="_blank"><i className="fab fa-instagram"></i></a>
-      </div>
-      <div>
         <a href="https://twitter.com" rel="noreferrer" target="_blank"><i className="fab fa-twitter"></i></a>
-      </div>
-      <div>
         <a href="https://pinterest.com" rel="noreferrer" target="_blank"><i className="fab fa-pinterest"></i></a>
+        <a href="https://www.youtube.com" rel="noreferrer" target="_blank"><i className="fab fa-youtube"></i></a>
       </div>
       <div>
-        <a href="https://www.youtube.com" rel="noreferrer" target="_blank"><i className="fab fa-youtube"></i></a>
+        <p>Copyright &copy; 2021 Wiola Polok www.u-v.codes. All Rights Reserved</p>
       </div>
     </footer>
   );
